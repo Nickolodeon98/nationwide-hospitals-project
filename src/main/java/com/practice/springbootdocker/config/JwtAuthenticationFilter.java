a6be7fdd2380c95ca2b326a690452a906a05ecb7
Nickolodeon98 <sjeon0730@gmail.com>
2023-03-14 07:30:31 +0900

    feat: 보안 필터체인에서 사용할 JWT 토큰 필터 구현

---
+++ src/main/java/com/practice/springbootdocker/config/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.practice.springbootdocker.config;
+
+import com.practice.springbootdocker.service.UserService;
+import com.practice.springbootdocker.utils.JwtTokenProvider;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private final UserService userService;
+  private final String secretKey;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+    if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+    String token = "";
+
+    try {
+      token = authorizationHeader.split(" ")[1];
+    } catch (Exception e) {
+      filterChain.doFilter(request, response);
+      throw new RuntimeException(e);
+    }
+
+
+
+
+  }
+}
