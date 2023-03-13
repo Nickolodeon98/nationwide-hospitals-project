f87ccbb1e86f85c70ffbb2b22a3cc1a686a4b80d
Nickolodeon98 <sjeon0730@gmail.com>
2023-03-14 07:09:49 +0900

    feat: JWT 를 생성하는 메서드 구현

--- src/main/java/com/practice/springbootdocker/utils/JwtTokenProvider.java
+++ src/main/java/com/practice/springbootdocker/utils/JwtTokenProvider.java
@@ -1,8 +1,29 @@
 package com.practice.springbootdocker.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtTokenProvider {
 
-  public static String createToken(String secretKey, String userName) {
+  private static final Long period = 1000L * 60 * 60;
 
+  @Value("${jwt.secret}")
+  private String secretKey;
+
+  public static String createToken(String secretKey, String userName) {
+    Claims claims = Jwts.claims().setSubject(userName);
+    Date currentDate = new Date();
+
+    return Jwts.builder()
+        .setClaims(claims)
+        .setIssuedAt(currentDate)
+        .setExpiration(new Date(currentDate.getTime() + period))
+        .signWith(SignatureAlgorithm.HS256, secretKey)
+        .compact();
   }
 }
