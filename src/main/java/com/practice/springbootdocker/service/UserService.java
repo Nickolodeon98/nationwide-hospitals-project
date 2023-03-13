2de97cbdb0ece5bbebfd5054c32bfc929abf3d05
Nickolodeon98 <sjeon0730@gmail.com>
2023-03-14 06:58:00 +0900

    feat: 사용자 정보가 주어지면 사용자를 인가하고 토큰을 발급하는 메서드 구현

--- src/main/java/com/practice/springbootdocker/service/UserService.java
+++ src/main/java/com/practice/springbootdocker/service/UserService.java
@@ -2,16 +2,30 @@ package com.practice.springbootdocker.service;
 
 import com.practice.springbootdocker.domain.dto.JoinRequest;
 import com.practice.springbootdocker.domain.dto.JoinResponse;
+import com.practice.springbootdocker.domain.dto.LoginRequest;
+import com.practice.springbootdocker.domain.dto.LoginResponse;
 import com.practice.springbootdocker.domain.entity.User;
+import com.practice.springbootdocker.repository.UserRepository;
+import com.practice.springbootdocker.utils.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 @RequiredArgsConstructor
 public class UserService {
 
   private final UserRepository userRepository;
 
+  @Value("${jwt.secret}")
+  private String secretKey;
+
   public JoinResponse registerUser(JoinRequest joinRequest) {
     User savedUser = userRepository.save(joinRequest.toEntity());
     return JoinResponse.of(savedUser);
+  }
+
+  public LoginResponse authenticateUser(LoginRequest loginRequest) {
+    String token = JwtTokenProvider.createToken(secretKey, loginRequest.getUserName());
+
+    return LoginResponse.builder().token(token).build();
   }
 }
